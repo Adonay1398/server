33cde0ff475812d0b4cd66aea48c6d9f499aeb8d
Adonay1398 <Luisbasket1398@gmail.com>
2024-11-13 13:17:42 -0600

    add selrializer to contructors and identifier

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer,ScoreConstructoSerializer
 from rest_framework.permissions import IsAuthenticated,AllowAny
+from .models import ScoreConstructo
+
+
+
 
 # Create your views here.
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (AllowAny,)
+    
+class ScoreCosntructoListViews(generics.ListCreateAPIView):
+    queryset = ScoreConstructo.objects.all()
+    serializer_class = ScoreConstructoSerializer
+    permission_classes = (IsAuthenticated,)
+    
+class ScoreCosntructoDetailViews(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ScoreConstructo.objects.all()
+    serializer_class = ScoreConstructoSerializer
+    permission_classes = (IsAuthenticated,)
+class ScoreIndicadoreListViews(generics.ListCreateAPIView):
+    queryset = ScoreConstructo.objects.all()
+    serializer_class = ScoreConstructoSerializer
+    permission_classes = (IsAuthenticated,)
+    
+class ScoreIndicadoreDetailViews(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ScoreConstructo.objects.all()
+    serializer_class = ScoreConstructoSerializer
+    permission_classes = (IsAuthenticated,)
