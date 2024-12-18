ee98f39997b4da4d9f2a15dde97252a548fe0e8c
Adonay1398 <Luisbasket1398@gmail.com>
2024-12-18 16:16:12 -0600

    changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -34,11 +34,11 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
 #SECRET_KEY = 'django-insecure-&%$jrf&gk0ern1(!%=xmeh)$1f$)f6p*p!rm=xsmr$27g4^9-y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = os.getenv('DEBUG', 'False') == 'True'
-DEBUG = True
-#ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','127.0.0.1:8000,server-prueba1.onrender.com').split(',')
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+#DEBUG = True
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','127.0.0.1:8000,server-prueba1.onrender.com').split(',')
 
-ALLOWED_HOSTS=['127.0.0.1']
+#ALLOWED_HOSTS=['127.0.0.1']
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": ( 
