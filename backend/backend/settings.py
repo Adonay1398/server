eaa030030261a87e4a8bc84bb3fe29294577662f
Adonay1398 <128860125+Adonay1398@users.noreply.github.com>
2024-12-19 09:50:17 -0600

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -36,9 +36,9 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 #DEBUG = True
-#ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','127.0.0.1:8000,server-prueba1.onrender.com').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','127.0.0.1:8000,server-prueba1.onrender.com').split(',')
 
-ALLOWED_HOSTS=['127.0.0.1']
+#ALLOWED_HOSTS=['127.0.0.1']
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": ( 
