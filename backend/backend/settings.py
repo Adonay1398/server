1edf7c0357ddbe808aaed5c9001da83bfb71cfa1
Adonay1398 <128860125+Adonay1398@users.noreply.github.com>
2024-12-21 17:38:31 -0600

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 #DEBUG = True
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','server-prueba1.onrender.com')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','127.0.0.1:8000,server-prueba1.onrender.com').split(',')
 
 #ALLOWED_HOSTS=['127.0.0.1']
 
