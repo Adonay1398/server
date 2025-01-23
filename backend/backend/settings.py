edae405569808bfc65d1fa4af337696a7214178a
Adonay1398 <Luisbasket1398@gmail.com>
2025-01-22 22:09:44 -0600

    Update CORS settings to include trailing comma for consistency

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -188,7 +188,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://demo-tutores.onrender.com",
-    "https://server-prueba1.onrender.com"
+    "https://server-prueba1.onrender.com",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
